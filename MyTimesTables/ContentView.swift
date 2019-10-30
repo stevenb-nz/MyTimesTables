51c2b2bd8458a572a03ba3449d50f6585f797b6d
Steven Brown <sgbrown@mac.com>
2019-10-30 16:36:20 +1300

    Initial Commit

---
+++ MyTimesTables/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyTimesTables
+//
+//  Created by Steven Brown on 30/10/19.
+//  Copyright © 2019 Steven Brown. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
